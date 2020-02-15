ee741561864e2238d4f46ac697f609c9b0fcfca7
Michael Lange <dingoeatingfuzz@gmail.com>
2020-02-15 13:35:09 -0800

    A simple component to emulate the piece of paper

---
+++ src/Frame.svelte
@@ -0,0 +1,15 @@
+<style>
+  div {
+    background: #17171a;
+    border: 1px solid #444;
+    border-radius: 2px;
+    width: 900px;
+    height: 900px;
+    box-shadow: 0 8px 15px -3px rgba(0, 0, 0, 0.4);
+    color: white;
+  }
+</style>
+
+<div>
+  <slot />
+</div>
